58f980fbcae1aa388f3b84c852e7c98f91f7945a
psforza <psforza@redhat.com>
2016-05-27 01:03:43 +0200

    xx

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var cc       = require('config-multipaas'),
-    finalhandler= require('finalhandler'),
-    http     = require("http"),
-    Router       = require('router'),
-    fs = require('fs'),
-    serveStatic       = require("serve-static");
-
+var cc              = require('config-multipaas'),
+    finalhandler    = require('finalhandler'),
+    http            = require("http"),
+    Router          = require('router'),
+    fs              = require('fs'),
+    serveStatic     = require("serve-static"),
+    express         = require('express'),
+    SimpleWebsocket = require('simple-websocket');
+ 
 var config   = cc();
 var app      = Router()
 
@@ -25,7 +27,9 @@ app.get("/", function (req, res) {
   res.end(index.toString())
 })
 
-//  Create server 
+app.use('/public', express.static('public'));
+
+// Create server 
 var server = http.createServer(function(req, res){
   var done = finalhandler(req, res)
   app(req, res, done)
@@ -33,4 +37,15 @@ var server = http.createServer(function(req, res){
 
 server.listen(config.get('PORT'), config.get('IP'), function () {
   console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+});
+
+// WebSocket
+var socket = new SimpleWebsocket('ws://echo.websocket.org')
+socket.on('connect', function () {
+  // socket is connected! 
+  socket.send('sup!')
+})
+ 
+socket.on('data', function (data) {
+  console.log('got message: ' + data)
 });
