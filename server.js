76cf939dd2055abd9b8afa448bcded35f484d9ff
psforza <psforza@redhat.com>
2016-05-27 12:25:37 +0200

    inserito ws con websocket package e modificato tutto il server

--- server.js
+++ server.js
@@ -1,64 +1,51 @@
-var cc              = require('config-multipaas'),
-    finalhandler    = require('finalhandler'),
-    http            = require("http"),
-    Router          = require('router'),
-    fs              = require('fs'),
-    serveStatic     = require("serve-static"),
-    express         = require('express'),
-    SimpleWebsocket = require('simple-websocket'),
-    WebSocketServer = require('ws').Server;
- 
-var config   = cc();
-var app      = Router()
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 
-// Serve up public/ftp folder 
-app.use(serveStatic('static'))
-
-// Routes
-app.get("/status", function (req, res) {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json; charset=utf-8')
-  res.end("{status: 'ok'}\n")
-})
-
-app.get("/", function (req, res) {
-  var index = fs.readFileSync(__dirname + '/index.html')
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end(index.toString())
-})
-
-app.use('/public', express.static('public'));
-
-// Create server 
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-  app(req, res, done)
-})
-
-server.listen(config.get('PORT'), config.get('IP'), function () {
-  console.log( "Listening on " + config.get('IP') + ", port " + config.get('PORT') )
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
 
-// WebSocket client
-//var socket = new SimpleWebsocket('ws://echo.websocket.org')
-//socket.on('connect', function () {
-//  socket.send('sup!')
-//})
-// 
-//socket.on('data', function (data) {
-//  console.log('got message: ' + data)
-//});
-
-//WebSocket server
-var wss = new WebSocketServer({server:server});
-
-wss.on('connection', function(ws) {
-    console.log(connection.utf8Data);
-    connection.sendUTF('hello from server');
-    setTimeout(function() {
-        connection.sendUTF('this is a websocket example');
-    }, 1000);
-    
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
 });
 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
